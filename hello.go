94020e8510df4c8181eb8b96678d1915fc912391
Nirbhay Singh <105772373+NirbhaySingh74@users.noreply.github.com>
2024-10-16 18:02:30 +0530

    variable in Go

--- hello.go
+++ hello.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+// func printname() {
+// 	fmt.Println("Hello, World", 7+10)
+// }
+
 func main() {
-    fmt.Println("Hello, World!")
+	var confrenceName = "Go Conference"
+	const confrenceTickets = 50
+	var remainingTickets = 50
+	fmt.Printf("Welcome to our %v booking application\n", confrenceName)
+	fmt.Println("We have total of", confrenceTickets, "tickets and", remainingTickets, "are still available.")
+	fmt.Println("Get your tickets here to attend")
 }
